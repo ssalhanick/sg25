1d59d629ec4f90fa1114157681c20892067d07ff
Scott Salhanick <ssalhanick@gmail.com>
2025-06-21 02:08:15 -0500

    deleting this and starting from scratch

--- plugins/sg-humanitix-importer/assets/src/js/editor.js
+++
@@ -1,8 +0,0 @@
-/**
- * Editor JavaScript for Humanitix Importer
- */
-
-import '../sass/editor.scss';
-
-// Your editor/admin JavaScript code here.
-console.log('sg-humanitix-importer editor loaded');
